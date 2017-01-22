713d5a27e79357745844d48ab5c1e0b051f88b21
Oskar Walker <oskar.walker@gmail.com>
2017-01-22 17:04:56 +0100

    Updated script to copy files from src/client/cordova to build-folder

--- scripts/prepare-cordova-ios.js
+++ scripts/prepare-cordova-ios.js
@@ -6,20 +6,38 @@ const shell = require('shelljs')
 const srcDir = path.join(__dirname, '../public/')
 const dstDir = path.join(__dirname, '../cordova/www/')
 
-try {
-  fs.statSync(srcDir)
-} catch (e) {
-  console.error('Path does not exist: ' + srcDir)
-  process.exit(2)
-}
+const htmlSrcDir = path.join(__dirname, '../client/src/cordova/')
 
+// Make sure dstDir exists
 try {
   fs.statSync(dstDir)
 } catch (e) {
   fs.mkdirSync(dstDir)
 }
 
-let code = shell.exec('rsync -ra --exclude="- .*" "' + srcDir + '" "' + dstDir + '"').code
+const paths = [srcDir, htmlSrcDir]
+
+paths.forEach(path => {
+  try {
+    fs.statSync(srcDir)
+  } catch (e) {
+    console.error('Path does not exist: ' + srcDir)
+    process.exit(2)
+  }  
+})
+
+let code
+
+// Copy public -> www
+code = shell.exec('rsync -ra --exclude="- .*" "' + srcDir + '" "' + dstDir + '"').code
+
+if (code !== 0) {
+  console.error('Error occured on copying public. Code: ' + code)
+  process.exit(3)
+}
+
+// Copy html/cordova -> www
+code = shell.exec('rsync -ra --exclude="- .*" "' + htmlSrcDir + '" "' + dstDir + '"').code
 
 if (code !== 0) {
   console.error('Error occured on copying public. Code: ' + code)
