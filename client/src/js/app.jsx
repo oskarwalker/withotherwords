cec185fbb6f929110eaf891dcdcc5004ea60aa06
Oskar Walker <oskar.walker@gmail.com>
2017-01-22 17:03:18 +0100

    Added support for a custom cordova build

--- client/src/js/app.jsx
+++ client/src/js/app.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { render } from 'react-dom'
 import ntp from 'socket-ntp/client/ntp.js'
 
-const socket = io.connect() // eslint-disable-line no-use-before-define
+const socket = io.connect(window.cordova ? 'https://wow.oskarwalker.se' : undefined) // eslint-disable-line no-use-before-define
 ntp.init(socket, { interval: 5000 })
 
 render(
