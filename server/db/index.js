cb132e4e34d343b5cc1ae665446e99c8ac34350b
Oskar Walker <oskar.walker@gmail.com>
2017-01-24 17:37:10 +0100

    Added check for index before creation

--- server/db/index.js
+++ server/db/index.js
@@ -1,11 +1,9 @@
-const rethinkdb = require('rethinkdb')
+const r = require('rethinkdb')
 const getWordCursor = require('../data/words')
 
-rethinkdb.setup = () => new Promise((resolve, reject) => {
-  rethinkdb.connect({ host: process.env.DB_HOST || 'localhost', port: 28015 })
-    .then((connection) => {
-      resolve(connection)
-    })
+r.setup = () => new Promise((resolve, reject) => {
+  r.connect({ host: process.env.DB_HOST || 'localhost', port: 28015 })
+    .then((connection) => resolve(connection))
     .catch(reject)
 })
 
@@ -52,7 +50,10 @@ const setupDatabase = (db, connection) => new Promise(async (resolve, reject) =>
   // Setup words
   try {
     // Create secondary index for category
-    await db.table('words').indexCreate('category').run(connection)
+    const indexList = await db.table('words').indexList().run(connection)
+    if(indexList.includes('category') === false) {
+      await db.table('words').indexCreate('category').run(connection)
+    }
 
     const tableEmpty = await db.table('words').count().run(connection) === 0
     const { cacheExists, getData } = await getWordCursor()
@@ -74,6 +75,6 @@ const setupDatabase = (db, connection) => new Promise(async (resolve, reject) =>
 })
 
 module.exports = {
-  db: rethinkdb,
+  db: r,
   setupDatabase
 }
