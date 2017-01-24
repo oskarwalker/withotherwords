ee64cb27880e34b2e9eacda29dc7bb60c56c0cd5
Oskar Walker <oskar.walker@gmail.com>
2017-01-24 16:25:03 +0100

    Fixed database creation check at server start

--- server/db/index.js
+++ server/db/index.js
@@ -12,12 +12,12 @@ rethinkdb.setup = () => new Promise((resolve, reject) => {
 const setupDatabase = (db, connection) => new Promise(async (resolve, reject) => {
   // Setup Database
   const dbName = 'wow'
-  const dbList = await db.dbList()
+  const dbList = await db.dbList().run(connection)
 
-  if (dbList.contains(dbName) === false) {
+  if (dbList.includes(dbName) === false) {
     try {
       await db.dbCreate(dbName).run(connection)
-    } catch (ex) { 
+    } catch (ex) {
       reject(ex)
       return
     }
@@ -26,29 +26,34 @@ const setupDatabase = (db, connection) => new Promise(async (resolve, reject) =>
   connection.use(dbName)
 
   // Setup tables
-  const tableList = db.tableList()
+  const tableList = await db.tableList().run(connection)
   const tableNames = [
     'games',
     'words'
   ]
 
-  tableNames.forEach(async tableName => {
-    if (tableList.contains(tableName) === true) {
-      return
-    }
+  let tableCreatePromises = []
 
-    try {
-      await db.tableCreate(tableName).run(connection)
-    } catch (ex) {
-      if (ex.msg.indexOf('exists') === -1) {
-        reject(ex)
-        return
-      }
+  tableNames.forEach(tableName => {
+    if (tableList.includes(tableName) === false) {
+      tableCreatePromises.push(db.tableCreate(tableName).run(connection))
     }
   })
 
+  try {
+    await Promise.all(tableCreatePromises)
+  } catch (ex) {
+    if (ex.msg.indexOf('exists') === -1) {
+      reject(ex)
+      return
+    }
+  }
+
   // Setup words
   try {
+    // Create secondary index for category
+    await db.table('words').indexCreate('category').run(connection)
+
     const tableEmpty = await db.table('words').count().run(connection) === 0
     const { cacheExists, getData } = await getWordCursor()
 
