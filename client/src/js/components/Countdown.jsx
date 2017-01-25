bd64f309629283fbfc541c81d9678bca720e1080
Oskar Walker <oskar.walker@gmail.com>
2017-01-25 10:19:07 +0100

    Changed Countdown logic and displayed decimals

--- client/src/js/components/Countdown.jsx
+++ client/src/js/components/Countdown.jsx
@@ -4,16 +4,12 @@ import timer from 'react-timer-hoc'
 const CountdownBase = ({ timer, synchronizeWith, startTime = 60 }) => {
   const now = Date.now()
 
-  if (now - synchronizeWith >= (startTime - 5) * 1000 && timer.delay !== 100) {
-    timer.setDelay(100)
-  }
-
   if (now - synchronizeWith >= startTime * 1000) {
     timer.stop()
   }
 
   const remaining = (now - synchronizeWith) / 1000
-  return <div>{ (Math.max(0, startTime - remaining).toFixed(1)) }</div>
+  return <div>{ (Math.max(0, startTime - remaining).toFixed(0)) }</div>
 }
 
 const Countdown = timer(1000)(CountdownBase)
