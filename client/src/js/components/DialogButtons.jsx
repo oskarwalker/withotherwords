410bde2e7db110e76f2abd5569c77dbaefe5cb4b
danhide <daniel.hidesten@hotmail.com>
2017-02-02 12:50:14 +0100

    changed order on dialog buttons

--- client/src/js/components/DialogButtons.jsx
+++ client/src/js/components/DialogButtons.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const DialogButtons = ({onOk, onCancel }) => {
 	return(
 		<div className="dialog-buttons">
-      <button className="dialog-button-ok" onClick={onOk}>Anslut</button>
-      <button className="dialog-button-cancel" onClick={onCancel}>Avbryt</button> 
+      <button className="dialog-button dialog-button--left dialog-button-cancel" onClick={onCancel}>Avbryt</button> 
+      <button className="dialog-button dialog-button--right dialog-button-ok" onClick={onOk}>Anslut</button>
     </div>
 	)
 }
