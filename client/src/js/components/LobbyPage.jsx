d5fb25c95161e1b0588c9c44cc27536cb9999d37
Oskar Walker <oskar.walker@gmail.com>
2017-02-06 20:16:30 +0100

    Styled LobbyPage

--- client/src/js/components/LobbyPage.jsx
+++ client/src/js/components/LobbyPage.jsx
@@ -11,15 +11,14 @@ const LobbyPage = ({ players, gameCode, gameOwnerId, isGameOwner }, { socket }) 
         ? <BackButton>Avsluta Spel</BackButton>
         : <BackButton>Lämna Spel</BackButton>
     }
-    <h1>Lobby page</h1>
-    <h2>Game code</h2>
-    <input type='text' value={gameCode} readOnly />
+    <input className='game-code' type='text' value={gameCode} readOnly />
+    <span className='sub-note'>Bjud in andra med denna kod</span>
     <ul className='players'>
-      {players.map(player => <li key={player.id}>{player.name}</li>)}
+      {players.map((player, index) => <li className="player-list-item" key={player.id}><span className="player-team">Lag {index + 1}</span><span className="player-name">{player.name}</span></li>)}
     </ul>
     {isGameOwner
       ? <button className="button-big button-bottom" disabled={players.length < 2} onClick={startGame.bind(null, socket)}>Starta Spel</button>
-      : <p>Väntar på att {players.find(player => player.id === gameOwnerId).name} ska starta spelet!</p>
+      : <p className="info-text-bottom">Väntar på att {players.find(player => player.id === gameOwnerId).name} ska starta spelet!</p>
     }
   </div>
 )
