65f2e418b6d42100c0eda3ddd55645474258b0dc
Oskar Walker <oskar.walker@gmail.com>
2017-02-06 18:40:53 +0100

    Increment wordIndex on givePoint

--- server/sockets/givePoint.js
+++ server/sockets/givePoint.js
@@ -1,10 +1,12 @@
+const safe = require('../lib/safe')
+
 async function givePoint (socket, db, connection, sessionId) {
-  const gamesCursor = await db
+  const [gamesCursorError, gamesCursor] = await safe(db
   .table('games')
   .filter(game => game('players').contains(player => player('sessionId').eq(sessionId)))
-  .run(connection)
+  .run(connection))
 
-  const games = await gamesCursor.toArray()
+  const [gamesError, games] = await safe(gamesCursor.toArray())
 
   if (games.length === 0) {
     socket.emit('gameError', 'You\'re not in a game. Can not give point.')
@@ -13,30 +15,32 @@ async function givePoint (socket, db, connection, sessionId) {
   const game = games.shift()
 
   if (
-    game.status === 'running' &&
-    game.roundEndTime > Date.now()
-  ) {
-    const currentPlayerId = game.currentPlayerId
-    const players = game.players
-
-    const newPlayers = players.map(player => {
-      if (player.id === currentPlayerId) {
-        return Object.assign({}, player, {
-          points: player.points + 1
-        })
-      } else {
-        return player
-      }
-    })
-
-    await db
-    .table('games')
-    .get(game.id)
-    .update({players: newPlayers})
-    .run(connection)
-  } else {
+    game.status !== 'running' ||
+    game.roundEndTime <= Date.now()
+  )  {
     socket.emit('gameError', 'The round has ended, can\'t give points now.')
+    return 
   }
+
+  const currentPlayerId = game.currentPlayerId
+  const players = game.players
+
+  const newPlayers = players.map(player => {
+    if (player.id === currentPlayerId) {
+      return Object.assign({}, player, {
+        points: player.points + 1
+      })
+    } else {
+      return player
+    }
+  })
+
+  const [updateError] = await safe(db
+  .table('games')
+  .get(game.id)
+  .update({players: newPlayers, wordIndex: game.wordIndex + 1})
+  .run(connection))
+
 }
 
 module.exports = givePoint
