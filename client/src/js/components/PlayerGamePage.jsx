c90bfd960ab5660daf72d6fa817c6120611a09a6
Oskar Walker <oskar.walker@gmail.com>
2017-02-06 17:38:18 +0100

    Added button to skip current word in player view

--- client/src/js/components/PlayerGamePage.jsx
+++ client/src/js/components/PlayerGamePage.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import GameTimer from './GameTimer.jsx'
 
-const PlayerGamePage = ({ synchronizeWith, offset, roundTime }) => (
-  <GameTimer
-    synchronizeWith={synchronizeWith}
-    offset={offset}
-    roundTime={roundTime}
-  />
+function skipWord (socket) {
+  socket.emit('skip-word')
+}
+
+const PlayerGamePage = ({ synchronizeWith, offset, roundTime }, { socket }) => (
+  <div className="player-game-page">
+    <GameTimer
+      synchronizeWith={synchronizeWith}
+      offset={offset}
+      roundTime={roundTime}
+    />
+    <button onClick={skipWord.bind(null, socket)}>Hoppa över</button>
+  </div>
 )
+
+PlayerGamePage.contextTypes = {
+  socket: React.PropTypes.object
+}
 
 export default PlayerGamePage
