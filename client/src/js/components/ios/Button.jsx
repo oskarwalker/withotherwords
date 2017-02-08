ad83535ecb0d5f44a17100b55b6853c70b4b4a25
Oskar Walker <oskar.walker@gmail.com>
2017-02-08 17:48:14 +0100

    Removed unused deps and components

--- client/src/js/components/ios/Button.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Button ({ content, onClick }) {
-
-}
-
-export default Button
--- client/src/js/components/ios/SVG.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SVG ({ svgPath, ...props }) {
-  return (
-    <img src={svgPath} {...props} />
-  )
-}
-
-export default SVG
