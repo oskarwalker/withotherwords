034db67ed5772d31c50d8d53b255b113e600b295
Daniel Hidesten <daniel.hidesten@hotmail.com>
2017-02-10 14:02:15 +0100

    Current word on referee page css added

--- client/src/js/components/RefereeGamePage.jsx
+++ client/src/js/components/RefereeGamePage.jsx
@@ -9,7 +9,7 @@ const RefereeGamePage = ({ synchronizeWith, offset, roundTime, currentWord }, { 
   <div className='referee-game-page'>
     <span className='time-left-label'>Tid kvar</span>
     <Countdown synchronizeWith={synchronizeWith} offset={offset} roundTime={roundTime} />
-    <span className='current-word' key={currentWord.id}>{currentWord.word}</span>
+    <span className='current-word-referee' key={currentWord.id}>{currentWord.word}</span>
     <span className='button-point-note'>Har de gissat rätt?</span>
     <button className='button-big button-point' onClick={givePoint.bind(null, socket)}>Ge poäng</button>
   </div>
