0eaef4af1ac96d15589b617642968f90e474ec94
Oskar Walker <oskar.walker@gmail.com>
2017-02-12 21:05:15 +0100

    Added players position on last page

--- client/src/js/components/FinishedPage.jsx
+++ client/src/js/components/FinishedPage.jsx
@@ -22,11 +22,11 @@ const FinishedPage = ({ players, isGameOwner }, { socket }) => {
       <button className='button-header button-right' onClick={replayGame.bind(null, socket)}>Spela igen</button>
       <div className='winner'>
         <ul className='players'>
-          {winner.map((player, index) => <li className='player-list-item' key={player.id}><h1>Vinnarna</h1><span className='player-points-wrapper'><span className='player-points'>{player.points}</span><span className='player-points-label'>Poäng</span></span><span className='player-name'>{player.name}</span><span className='player-team'>Lag {index + 1}</span></li>)}
+          {winner.map((player, index) => <li className='player-list-item' key={player.id}><h1>Vinnarna</h1><span className='player-points-wrapper'><span className='player-points'>{player.points}</span><span className='player-points-label'>Poäng</span></span><span className='player-name'>{player.name}</span><span className='player-team'>Plats {index + 1}</span></li>)}
         </ul>
       </div>
       <ul className='players'>
-        {sortedPlayers.map((player, index) => <li className='player-list-item' key={player.id}><span className='player-team'>Lag {index + 1}</span><span className='player-name'>{player.name}</span><span className='player-points-wrapper'><span className='player-points'>{player.points}</span><span className='player-points-label'>Poäng</span></span></li>)}
+        {sortedPlayers.map((player, index) => <li className='player-list-item' key={player.id}><span className='player-team'>Plats {index + 2}</span><span className='player-name'>{player.name}</span><span className='player-points-wrapper'><span className='player-points'>{player.points}</span><span className='player-points-label'>Poäng</span></span></li>)}
       </ul>
     </div>
   )
