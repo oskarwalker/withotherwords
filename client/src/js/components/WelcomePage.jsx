82e9fd09a3f17ec5cb90a610f66f549193de79c9
Daniel Hidesten <daniel.hidesten@hotmail.com>
2017-02-15 18:18:21 +0100

    Help page content and styling

--- client/src/js/components/WelcomePage.jsx
+++ client/src/js/components/WelcomePage.jsx
@@ -101,7 +101,18 @@ class WelcomePage extends Component {
         {this.state.isShowingHelpDialog &&
           <HelpDialogContainer onClick={this.helpDialogClose}>
             <div className='help-dialog-content'>
-              <p>test</p>
+              <h1>Med Andra Ord</h1>
+              <p>Med andra ord går ut på att man beskriver ett ord för sina lagkamrater som ska gissa rätt ord. Utmaningen ligger i att man får endast använda andra ord för att beskriva ordet.</p>
+              <p>I detta spel har varje lag varsin telefon med denna app installerad, det måste vara minst två lag.</p>
+              <blockquote>
+                <h3>Proffstips</h3>
+                <p>Välj antalet omgångar efter antalet lagmedlemmar, så alla får spela.</p>
+              </blockquote>
+              <p>Ett lag startar ett spel och sedan ansluter de andra lagen genom att ange spelets kod. Sedan kommer lagen i tur och ordning att förklara ordet som syns på skärmen, samtidigt som ett annat lag ger poäng. Det andra lagen kommer överens om vem som ger poäng.</p>
+              <div className='illustrations'>
+                <img src='/static/img/Screen_shot_player.png' />
+                <img src='/static/img/Screen_shot_referee.png' />
+              </div>
               <button className='button-header button-right' onClick={this.helpDialogClose}>Stäng</button>
             </div>
           </HelpDialogContainer>
