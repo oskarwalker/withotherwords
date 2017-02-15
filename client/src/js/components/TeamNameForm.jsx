d1c9aa539f35a11061f35000f448b4bc7b7db938
Oskar Walker <oskar.walker@gmail.com>
2017-02-15 11:08:24 +0100

    Added CTA on WelcomePage

--- client/src/js/components/TeamNameForm.jsx
+++ client/src/js/components/TeamNameForm.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 const TeamNameForm = ({ onChange }) => (
   <div className='team-name-form'>
+    <p className='note top-note'>Börja med att skriva in lagnamn</p>
     <input type='text' className='generic-text-input' placeholder='Lagnamn' onChange={onChange} />
   </div>
 )
