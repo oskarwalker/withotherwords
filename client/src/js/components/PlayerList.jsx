022ce87657f869136ab2bbd0eb454a71a9112030
Oskar Walker <oskar.walker@gmail.com>
2017-02-16 09:15:46 +0100

    Changed color for the player who's next up

--- client/src/js/components/PlayerList.jsx
+++ client/src/js/components/PlayerList.jsx
@@ -4,7 +4,7 @@ const PlayerList = ({ players, currentPlayerId, clientPlayer }) => {
   const renderedPlayers = players.map((player, index) => (
     <li className='player-list-item' key={player.id}>
       {player.id === currentPlayerId &&
-      <span className='player-next-up'>{clientPlayer.id === player.id ? 'Er tur' : 'Deras tur'}</span>
+      <span className={`player-next-up${clientPlayer.id !== player.id ? ' player-next-up--others' : ''}`}>{clientPlayer.id === player.id ? 'Er tur' : 'Deras tur'}</span>
       }
       <span className='player-team'>Lag {index + 1}</span>
       <span className='player-name'>{player.name}</span>
